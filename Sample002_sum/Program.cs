047cbe1dd8180dea1bfc72de9b7237b8b7bdaf8e
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 21:43:01 +0900

    sum project

---
+++ Sample002_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Calculating two digits
+
+Console.Write("Enter first INTEGER digit> ");
+int firstDigit = Console.ReadLine();
+Console.Write("You entered number ");
+Console.WriteLine(firstDigit);

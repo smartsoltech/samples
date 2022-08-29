6238b8c7830beb6f7da3f6e82a777a70bc2f2749
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 22:10:11 +0900

    if...else descicion added(sample005

---
+++ Sample005_Welcoming/Program.cs
@@ -0,0 +1,14 @@
+﻿// new project
+Console.WriteLine("Enter your name> ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.WriteLine(username);
+if (username.ToLower() == "andrey")
+{
+ Console.WriteLine("Welcome, Master!");
+}
+else
+{
+ Console.Write("Hello, ");
+ Console.WriteLine(username);
+}
